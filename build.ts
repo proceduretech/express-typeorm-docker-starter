c5a8ab9201ae13ac45bad9d453ce8e8639090644
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2019-02-07 13:24:58 +0530

    Removes .env reference from build.ts

--- build.ts
+++ build.ts
@@ -4,6 +4,5 @@ const outDir = config.compilerOptions.outDir;
 
 s.rm('-rf', outDir);
 s.mkdir(outDir);
-s.cp('.env', `${outDir}/.env`);
 s.mkdir('-p', `${outDir}/common/swagger`);
-s.cp('server/common/swagger/Api.yaml', `${outDir}/common/swagger/Api.yaml`);
+s.cp('src/core/swagger/Api.yaml', `${outDir}/common/swagger/Api.yaml`);
